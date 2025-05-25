b6ab25eb3bbe00547d6d261cea0deb0de9ff0893
Rubevaldo <rubevaldoj@gmail.com>
2025-05-25 15:20:18 -0300

    Conexão com o banco e mapeamento das entidades

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjecoesPoC.Models;
+
+namespace ProjecoesPoC.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<CruzamentoSexoLocal> CruzamentoSexoLocals {  get; set; }
+        public DbSet<CruzamentoLocalIdade> CruzamentoLocalIdades { get; set; }
+        public DbSet<CruzamentoSexoIdade> CruzamentoSexoIdades { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CruzamentoSexoIdade>().Property(csi => csi.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<CruzamentoLocalIdade>().Property(csi => csi.Id).ValueGeneratedOnAdd();
+            modelBuilder.Entity<CruzamentoSexoIdade>().Property(csi => csi.Id).ValueGeneratedOnAdd();
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
