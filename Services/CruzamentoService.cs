284b046df6830d29396a49e8b98ded7cb8306d01
Rubevaldo <rubevaldoj@gmail.com>
2025-05-25 15:56:43 -0300

    Services - Lógica de Cruzamento de informações

---
+++ Services/CruzamentoService.cs
@@ -0,0 +1,46 @@
+﻿using ProjecoesPoC.Models;
+
+namespace ProjecoesPoC.Services
+{
+    public class CruzamentoService
+    {
+        public static List<CruzamentoSexoIdade> CruzamentoPorSexoIdade(List<InputData> dados)
+        {
+            return [.. dados
+                .GroupBy(p => new { p.Sexo, p.Idade })
+                .Select(grupo => new CruzamentoSexoIdade
+                {
+                    Sexo = grupo.Key.Sexo,
+                    Idade = grupo.Key.Idade,
+                    Total = grupo.Sum(p => p.Total)
+                })
+                .OrderBy(cru => cru.Idade).ThenBy(cru => cru.Sexo)];
+        }
+
+        public List<CruzamentoLocalIdade> CruzamentoPorLocalIdade(List<InputData> dados)
+        {
+            return [.. dados
+                .GroupBy(p => new { p.Local, p.Idade })
+                .Select(grupo => new CruzamentoLocalIdade
+                {
+                    Local = grupo.Key.Local,
+                    Idade = grupo.Key.Idade,
+                    Total = grupo.Sum(p => p.Total)
+                })
+                .OrderBy(cru => cru.Local).ThenBy(cru => cru.Idade)];
+        }
+
+        public List<CruzamentoSexoLocal> CruzamentoPorSexoLocal(List<InputData> dados)
+        {
+            return [.. dados
+                .GroupBy(p => new { p.Sexo, p.Local })
+                .Select(grupo => new CruzamentoSexoLocal
+                {
+                    Local = grupo.Key.Local,
+                    Sexo = grupo.Key.Sexo,
+                    Total = grupo.Sum(p => p.Total)
+                })
+                .OrderBy(cru => cru.Local).ThenBy(cru => cru.Sexo)];
+        }
+    }
+}
